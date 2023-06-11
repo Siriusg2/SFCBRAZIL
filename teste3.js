3601f4862762ff7e43fd591dc6f4a7ccffe48eb2
Gibson Gil <114748801+Siriusg2@users.noreply.github.com>
2023-06-11 11:59:15 -0500

    test 3 finished

--- teste3.js
+++ teste3.js
@@ -1,15 +1,23 @@
-var data =  require("./fakeData");
+const data = require("./fakeData");
 
-module.exports = function(req, res) {
-  
-    var name =  req.query.name;
+module.exports = function (req, res) {
+  const { name } = req.query;
 
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            data[i] = null;
-        }
-    }
+  /*we verify 
+  that databank have registries to prevent
+   a error server through ternarie operator in the 
+   find method(this verify that there are indexes that are diferent the null )  */
 
-    res.send("success");
+  const savedUser = data.find((user) => user?.name === name);
 
+  //if theren't are any registry that matches with the query, then we send status 404 not found
+  if (!savedUser) return res.status(404).send(`user ${name} not found`);
+
+  /*else we change the index of the databank that matches with the query with the reserved word null
+  and send status code 200 to let the user know that the registry was deteleted successfully
+  */
+  const indexOfUser = data.indexOf(savedUser);
+  data[indexOfUser] = null;
+
+  return res.status(200).send("success");
 };
