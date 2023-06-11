fd3b0e65432f98ed4813b73865b3958f299cf87a
Gibson Gil <114748801+Siriusg2@users.noreply.github.com>
2023-06-11 12:17:41 -0500

    test 4 finished

--- teste4.js
+++ teste4.js
@@ -1,13 +1,30 @@
-var data =  require("./fakeData");
+const data = require("./fakeData");
 
-module.exports =  function(req, res) {
-  
-    var id =  req.query.id;
+module.exports = function (req, res) {
+  //destructuring each parameter of the request body to be sent to fake data
+  const { id } = req.query;
+  const { name, job } = req.body;
 
-    const reg = data.find(d => id == id);
-    reg.name = req.body.name;
-    reg.job = req.body.job;
+  const reg = data.find((d) => id == id);
+  if (!reg)
+    return res
+      .status(404)
+      .send({ message: `no user found with that id ${id}` });
 
-    res.send(reg);
+  //valitations for each parameter
+  //verify type of the each parameter
+  if (typeof name !== "string")
+    return res.status(400).send("The name of the new user must be a string");
+  if (typeof job !== "string")
+    return res.status(400).send("The job of the new user must be a string");
 
+  //verify that the parameter are a string with characters, and not empty spaces
+  if (!name.trim())
+    return res.status(400).send("You must provide a new name for the  user");
+  if (!job.trim())
+    return res.status(400).send("You must provide a new job for the  user");
+  reg.name = name;
+  reg.job = job;
+
+  return res.status(200).send(reg);
 };
